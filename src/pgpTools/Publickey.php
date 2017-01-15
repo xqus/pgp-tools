c3efba08af9c924c4b549e438e5958a994f15c24
Audun Larsen <larsen@xqus.com>
2017-01-15 10:01:26 +0100

    Code cleanup and documentation for Publickey

--- src/pgpTools/Publickey.php
+++ src/pgpTools/Publickey.php
@@ -2,15 +2,42 @@
 
 namespace xqus\pgpTools;
 
+/**
+ * @author    Audun Larsen <larsen@xqus.com>
+ * @copyright Copyright (c) Audun Larsen, 2017
+ * @link      https://github.com/xqus/pgp-tools
+ * @license   http://opensource.org/licenses/mit-license.php The MIT License
+ */
+
+/**
+ * Provides object that represents public keys.
+ */
 class Publickey {
 
-  var $fingerprint = null;
-  var $keylen      = null;
-  var $created     = null;
-  var $expires     = null;
-  var $uids        = null;
+  public $fingerprint = null;
+  public $keylen      = null;
+  public $created     = null;
+  public $expires     = null;
+  public $uids        = null;
 
-  function __construct($key) {
+  /**
+   * Parse an array containging key-data and create the Publickey object.
+   *
+   * @param array $key
+   *   Array containing key data.
+   *   array(
+   *     'fingerprint' => 'Key fingerprint',
+   *     'keylen'      => 'Key length',
+   *     'created'     => 'UNIX timestamp for key creation date',
+   *     'expires'     => 'UNIX timestamp for key expire date',
+   *     'uids'        => array( // Key user ids
+   *                        'Name (comment) <email>'
+   *                      ),
+   *   )
+   *
+   * @return void
+   */
+  public function __construct($key) {
     foreach($key as $k => $v) {
       if(!empty($this->$k)) {
         throw new \Exception($k .' is not allowed as a key in public key array');
@@ -22,17 +49,16 @@ class Publickey {
     $this->setUids($this->uids);
   }
 
-  function setUids($uids) {
+  /**
+   * Loop trough the raw user IDs and convert to a Uid object.
+   *
+   * @param array $uids
+   * @return void
+   */
+  private function setUids($uids) {
     $this->uids = array();
     foreach($uids as $uid) {
       $this->uids[] = new Uid($uid);
     }
   }
-
-  function uids() {
-
-  }
-
-
-
 }
